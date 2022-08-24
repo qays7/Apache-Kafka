4d140f0ddf2f72b573e090b7d14849d81e8b914c
qays <qays.raed17@gmail.com>
2022-08-24 10:10:13 +0300

    new app

---
+++ microservices-demo/src/test/java/com/microservices/demo/MicroservicesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
